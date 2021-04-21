d953923e7865842f85223b4ab8e0283f206a6c20
Ryan Miller <ryandmiller3521@gmail.com>
2021-04-21 13:59:41 -0600

    Add fetchs to grab inital data

--- src/index.js
+++ src/index.js
@@ -7,4 +7,43 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const getDestinationData = () => fetch("http://localhost:3001/api/v1/destinations")
+    .then(response => checkForError(response))
+    .catch(err => console.log(`User API Error: ${err.message}`));
+
+const getTravelerData = () => fetch("http://localhost:3001/api/v1/travelers")
+    .then(response => checkForError(response))
+    .catch(err => console.log(`Ingredients API Error: ${err.message}`));
+
+const getTripData = () => fetch("http://localhost:3001/api/v1/trips")
+    .then(response => checkForError(response))
+    .catch(err => console.log(`Recipe API Error: ${err.message}`));
+
+function getData() {
+    return Promise.all([getTravelerData(), getTripData(), getDestinationData()])
+}
+
+const checkForError = response => {
+    if (!response.ok) {
+        throw new Error('Something went wrong, please try again.');
+    } else {
+        return response.json();
+    }
+}
+
+let travelerData, tripData, destinationData;
+
+
+window.onload = onStartup();
+
+function onStartup() {
+    getData()
+        .then(([getTravelerData, getTripData, getDestinationData])=> {
+            travelerData = getTravelerData;
+            tripData = getTripData;
+            destinationData = getDestinationData;
+            console.log(travelerData);
+            console.log(tripData);
+            console.log(destinationData);
+        });
+}
