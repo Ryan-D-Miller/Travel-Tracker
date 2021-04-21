219b90a232a3094b7de326a87043dfa3def86eca
Ryan Miller <ryandmiller3521@gmail.com>
2021-04-21 15:45:48 -0600

    Add inital contructor for User class to take in traveler Information

--- src/user.js
+++ src/user.js
@@ -1,5 +1,9 @@
 class User {
-
+    constructor(traveler) {
+        this.id = traveler.id;
+        this.name = traveler.name;
+        this.travelerType = traveler.travelerType;
+    }
 }
 
 export default User;
--- test/user-test.js
+++ test/user-test.js
@@ -3,8 +3,46 @@ const expect = chai.expect;
 
 import User from '../src/User';
 
+const travelersTestData = [{
+    "id": 1,
+    "name": "Ham Leadbeater",
+    "travelerType": "relaxer",
+    }, {
+        "id": 2,
+        "name": "Rachael Vaughten",
+        "travelerType": "thrill-seeker",
+    }, {
+        "id": 3,
+        "name": "Sibby Dawidowitsch",
+        "travelerType": "shopper",
+    }, {
+        "id": 4,
+        "name": "Leila Thebeaud",
+        "travelerType": "photographer",
+    }, {
+        "id": 5,
+        "name": "Tiffy Grout",
+        "travelerType": "thrill-seeker",
+    }, {
+        "id": 6,
+        "name": "Laverna Flawith",
+        "travelerType": "shopper",
+    }, {
+        "id": 7,
+        "name": "Emmet Sandham",
+        "travelerType": "relaxer",
+    }];
+let user;
 describe('User', function() {
+    beforeEach(function() {
+        user = new User(travelersTestData[2]);
+    });
     it('should be a function', function() {
         expect(User).to.be.a('function');
+    });
+    it('should take in traveler data and have and id a name and travelerType', function() {
+        expect(user.id).to.equal(3);
+        expect(user.name).to.equal("Sibby Dawidowitsch");
+        expect(user.travelerType).to.equal("shopper");
     });
 });
