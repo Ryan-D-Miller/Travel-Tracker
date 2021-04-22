d64a1ae2811206f5f6301eef154d7d37e018d536
Ryan Miller <ryandmiller3521@gmail.com>
2021-04-22 14:14:03 -0600

    Make text easier to see

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -11,13 +11,13 @@ let domUpdates = {
         cardArea.innerHTML = "";
         user.trips.forEach(trip => {
             cardArea.insertAdjacentHTML('afterbegin', `
-                <section class="trip-card" style="background-image: url('${trip.destinationInfo.image}');">
-                    <header class="trip-header">
+                <section class="trip-card" style="background-image: linear-gradient(rgba(95, 95, 95, 0.4), rgba(159, 183, 199, 0.4)), url('${trip.destinationInfo.image}');">
+                <section class=${trip.status}> <strong>Status: ${trip.status}</strong></section>
+                <header class="trip-header">
                         <strong>${trip.destinationInfo.destination}</strong>
                         <p>Trip Cost: $${user.tripCost(trip)}</p>
                     </header>
                     <div class="trip-body">
-                        <strong>Status: ${trip.status}</strong>
                         <p>Number of Travelers: ${trip.travelers}</p>
                         <p>Departure Date: ${trip.date}</p>
                         <p>Days on Vacation: ${trip.duration}</p>
